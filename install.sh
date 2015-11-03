5eba0e976de37ab194b89de830b389e753a72959
Frans Peter Larsen <larsen.frans@gmail.com>
2015-11-03 16:35:45 +0100

    git, irssi

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 # /bin/sh
-sudo apt-get install htop curl wget blender build-essentials chromium-browser git pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu
+
+sudo apt-get install htop curl wget blender build-essential irssi screen chromium-browser git pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu
