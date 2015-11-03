676439c4de6d8d3f0f4e6f1271eb585fd53b842e
Frans Peter Larsen <larsen.frans@gmail.com>
2015-11-03 17:09:21 +0100

    mono, asp.net

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 # /bin/sh
 
-# Install download packages, download dotfiles and install em
-sudo apt-get install wget git curl
-wget https://github.com/webpro/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'
-sh $HOME/.dotfiles/install.sh
-
 # Install all the junk I need
-sudo apt-get install blender build-essential irssi screen chromium-browser git pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
+sudo apt-get wget git curl install blender build-essential irssi screen chromium-browser git pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
 
 # Install Mono
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
