2c3b55e26cf04b4a246f3b23a8f7c365e1167782
Frans Larsen <larsen.frans@gmail.com>
2015-11-05 13:38:03 +0100

    More readme Trying to figure out sudo, but having some trouble!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 My dotfiles and installation script for different things I use (Currently specific for my Laptop)
+
+# Installation
+* chmod +x install.sh
+* bash install.sh
--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-# /bin/sh
+#!/usr/bin/env bash
+
+set -e
 
 # Install all the junk I need
-apt-get --assume-yes install wget git curl blender build-essential irssi screen chromium-browser git gvfs-bin pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
+sudo apt-get --assume-yes install nodejs npm cmake bzr rpm wget git curl blender build-essential irssi screen chromium-browser git gvfs-bin pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
 
 # Install Mono
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian beta main" | tee /etc/apt/sources.list.d/mono-xamarin.list
-apt-get update && apt-get --assume-yes install mono-complete mono-devel ca-certificates-mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian beta main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update && sudo apt-get --assume-yes install mono-complete mono-devel ca-certificates-mono
 
 # Install ASP.NET
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
@@ -15,21 +17,21 @@ dnvm upgrade -r mono
 
 # Install Atom
 wget https://atom.io/download/deb -O atom.deb
-dpkg -i atom.deb
+sudo dpkg -i atom.deb
 rm atom.deb
-
-# Install Omnisharp for Atom
-apm install omnisharp-atom
 
 # Install Unity3D
 wget http://download.unity3d.com/download_unity/unity-editor-5.1.0f3+2015091501_amd64.deb -O unity-editor.deb
-dpkg -i unity-editor.deb
+sudo dpkg -i unity-editor.deb
 rm unity-editor.deb
 
 # Install SmartGit
 wget http://www.syntevo.com/smartgit/download?file=smartgit/smartgit-7_0_3.deb -O smartgit.deb
 dpkg -i smartgit.deb
 rm smartgit.deb
+
+# Set zsh as default shell
+chsh -s /usr/bin/zsh
 
 echo "Finished installing everything together with dotfiles!"
 echo "Happy coding $USER"
