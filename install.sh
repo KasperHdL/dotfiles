db6b59693940225d8773445b6066758c569f6ce8
Frans Larsen <larsen.frans@gmail.com>
2015-11-05 14:51:04 +0100

    Added missing scripts for Unity3d (Might cause issues, will test later)

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 set -e
 
 # Install all the junk I need
-sudo apt-get --assume-yes install nodejs npm cmake bzr rpm wget git curl blender build-essential irssi screen chromium-browser git gvfs-bin pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
+sudo apt-get -y install nodejs npm lib32gcc1 lib32stdc++ lib32stdc++6 libc6-i386 libpq5 ubuntu-restricted-extras cmake bzr rpm wget git curl blender build-essential irssi screen chromium-browser git gvfs-bin pm-utils make automake powertop python thermald ttf-ancient-fonts unzip zsh qemu libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
 
 # Install Mono
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian beta main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update && sudo apt-get --assume-yes install mono-complete mono-devel ca-certificates-mono
+sudo apt-get update && sudo apt-get -y install mono-complete mono-devel ca-certificates-mono
 
 # Install ASP.NET
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
@@ -21,13 +21,13 @@ sudo dpkg -i atom.deb
 rm atom.deb
 
 # Install Unity3D
-wget http://download.unity3d.com/download_unity/unity-editor-5.1.0f3+2015091501_amd64.deb -O unity-editor.deb
-sudo dpkg -i unity-editor.deb
+wget http://download.unity3d.com/download_unity/unity-editor-5.1.0f3+2015091501_amd64.deb -O unityeditor.deb
+sudo dpkg -i unityeditor.deb
 rm unity-editor.deb
 
 # Install SmartGit
 wget http://www.syntevo.com/smartgit/download?file=smartgit/smartgit-7_0_3.deb -O smartgit.deb
-dpkg -i smartgit.deb
+sudo dpkg -i smartgit.deb
 rm smartgit.deb
 
 # Set zsh as default shell
